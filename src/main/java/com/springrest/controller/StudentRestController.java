c36a1458eae6fa52bafae34bab5d3198560b6c34
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-03-27 12:56:46 +0200

    Update Rest Service To Throw Exception If Student Not Found Closes #6

--- src/main/java/com/springrest/controller/StudentRestController.java
+++ src/main/java/com/springrest/controller/StudentRestController.java
@@ -26,7 +26,7 @@ public class StudentRestController {
     }
 
     @GetMapping("/students")
-    public List<Student> getStudents(){
+    public List<Student> getStudentss(){
 
         return students;
     }
