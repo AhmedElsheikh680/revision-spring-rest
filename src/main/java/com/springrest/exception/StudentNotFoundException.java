abf6dbfdcaece38bc3fbff71c428f7ca77cdf464
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-03-27 12:49:56 +0200

    Handle Exception(StudentNotFoundException) Closes #5

---
+++ src/main/java/com/springrest/exception/StudentNotFoundException.java
@@ -0,0 +1,15 @@
+package com.springrest.exception;
+
+public class StudentNotFoundException  extends RuntimeException{
+    public StudentNotFoundException(String s) {
+        super(s);
+    }
+
+    public StudentNotFoundException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+    public StudentNotFoundException(Throwable throwable) {
+        super(throwable);
+    }
+}
